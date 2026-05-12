35ece011e3d1207607796701d2a80639c3c2a58f
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-05-12 16:59:31 -0600

    fear: Se agrego buscar de sintaxis en VentaController,VentaRepository,IVentaService,VentaService en listarVenta.html se agrego el boton de buscar

--- src/main/java/com/angelchacon/kinalapp/service/IVentaService.java
+++ src/main/java/com/angelchacon/kinalapp/service/IVentaService.java
@@ -1,14 +1,15 @@
 package com.angelchacon.kinalapp.service;
 
 import com.angelchacon.kinalapp.entity.Venta;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface IVentaService {
-
     List<Venta> listarTodos();
     Venta guardar(Venta venta);
     Optional<Venta> buscarPorCodigo(Integer codigo);
     void eliminar(Integer codigo);
+
+    // Nuevo método para el buscador
+    List<Venta> buscarVentas(String termino);
 }
